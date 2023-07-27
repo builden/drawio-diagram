10cbd7255b1cdf30fbc2764291f9f2f08fc08800
builden <billdeng@qq.com>
2023-07-27 18:04:04 +0800

    update example

--- encode-url.js
+++ encode-url.js
@@ -1,6 +1,6 @@
 /* cSpell:disable */
 const prefix = 'https://builden.tech/proxy/raw.githubusercontent.com/builden/drawio-diagram/main/';
-const url = prefix + 'HappyDraw.drawio.svg'; // <= Copy Relative Path
+const url = prefix + 'brands.drawio'; // <= Copy Relative Path
 
 console.log(encodeURIComponent(url));
-// => https%3A%2F%2Fbuilden.tech%2Fproxy%2Fraw.githubusercontent.com%2Fbuilden%2Fdrawio-diagram%2Fmain%2FHappyDraw.drawio.svg
+// => https%3A%2F%2Fbuilden.tech%2Fproxy%2Fraw.githubusercontent.com%2Fbuilden%2Fdrawio-diagram%2Fmain%2Fbrands.drawio
