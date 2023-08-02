b1cf3f197def270e70947aad59b3792873ba769f
builden <billdeng@qq.com>
2023-08-02 18:06:13 +0800

    add tao.drawio

--- encode-url.js
+++ encode-url.js
@@ -1,6 +1,6 @@
 /* cSpell:disable */
 const prefix = 'https://builden.tech/proxy/raw.githubusercontent.com/builden/drawio-diagram/main/';
-const url = prefix + 'brands.drawio'; // <= Copy Relative Path
+const url = prefix + 'philosophy/proposition.drawio'; // <= Copy Relative Path
 
-console.log(encodeURIComponent(url));
+console.log('https://viewer.builden.tech/#U' + encodeURIComponent(url));
 // => https%3A%2F%2Fbuilden.tech%2Fproxy%2Fraw.githubusercontent.com%2Fbuilden%2Fdrawio-diagram%2Fmain%2Fbrands.drawio
