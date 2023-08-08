b6ac0ef1a16d9a64262366126f328ae2109f9c03
builden <billdeng@qq.com>
2023-08-09 01:53:06 +0800

    use new url model

--- encode-url.js
+++ encode-url.js
@@ -1,6 +1,6 @@
 /* cSpell:disable */
-const prefix = 'https://builden.tech/proxy/raw.githubusercontent.com/builden/drawio-diagram/main/';
-const url = prefix + 'philosophy/proposition.drawio'; // <= Copy Relative Path
+const prefix = 'builden/drawio-diagram/main/';
+const url = prefix + 'philosophy/tao.drawio'; // <= Copy Relative Path
 
-console.log('https://viewer.builden.tech/#U' + encodeURIComponent(url));
-// => https%3A%2F%2Fbuilden.tech%2Fproxy%2Fraw.githubusercontent.com%2Fbuilden%2Fdrawio-diagram%2Fmain%2Fbrands.drawio
+console.log('https://viewer.builden.tech/g/' + encodeURIComponent(url));
+// => https://viewer.builden.tech/g/builden%2Fdrawio-diagram%2Fmain%2Fphilosophy%2Ftao.drawio
